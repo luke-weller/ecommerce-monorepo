e04768445d7233f48abd247c6d592e85a10ec805
luke-weller <wellerluke@hotmail.com>
2024-03-19 10:53:03 -0400

    more cleanup

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const createProduct = require("../contollers/productControllers/createProduct");
-const getAllProducts = require("../contollers/productControllers/getAllProducts");
-const getProductById = require("../contollers/productControllers/getProductById");
-const updateProduct = require("../contollers/productControllers/updateProduct");
-const deleteProduct = require("../contollers/productControllers/deleteProduct");
+const createProduct = require("../contollers/productControllers/createProductController");
+const getAllProducts = require("../contollers/productControllers/getAllProductsController");
+const getProductById = require("../contollers/productControllers/getProductByIdController");
+const updateProduct = require("../contollers/productControllers/updateProductController");
+const deleteProduct = require("../contollers/productControllers/deleteProductController");
 
 router.get("/", getAllProducts);
 router.get("/:productId", getProductById);
