453893b4b8069347e7f8d24a8f531a9fae086089
luke-weller <wellerluke@hotmail.com>
2024-04-10 14:51:42 -0400

    update users controller path

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const registerUser = require("../contollers/usersController/registerUserController");
-const deleteUser = require("../contollers/usersController/deleteUserController");
-const getUserByEmail = require("../contollers/usersController/getUserByEmailController");
-const updateUser = require("../contollers/usersController/updateUserController");
+const registerUser = require("../contollers/userControllers/registerUserController");
+const deleteUser = require("../contollers/userControllers/deleteUserController");
+const getUserByEmail = require("../contollers/userControllers/getUserByEmailController");
+const updateUser = require("../contollers/userControllers/updateUserController");
 
 router.post("/register", registerUser);
 router.delete("/:userId", deleteUser);
